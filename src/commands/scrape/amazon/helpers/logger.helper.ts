82fe8d2ffc0ef7fd6aaf4bf4b0587e8d28e209d3
Marco Franke <disane@icloud.com>
2023-02-07 12:45:37 +0100

    style(logs): :technologist: added context of execution to logs

--- src/commands/scrape/amazon/helpers/logger.helper.ts
+++ src/commands/scrape/amazon/helpers/logger.helper.ts
@@ -1,26 +1,30 @@
 import * as path from 'path';
 import winston, { format } from "winston";
-const { timestamp, printf } = format;
-const myFormat = printf(({ level, message, timestamp }) => {
-    return `[${timestamp}] ${level}: ${message}`;
-});
+const { timestamp, printf, label } = format;
 
-export const createLogger = (logLevel: string, logPath: string) => winston.createLogger({
-    level: logLevel.toString(),
-    // defaultMeta: { service: 'user-service' },
-    transports: [
-        new winston.transports.File({ filename: path.join(logPath, `error.log`).normalize(), level: `error` }),
-        new winston.transports.File({ filename: path.join(logPath, `verbose.log`).normalize(), level: `verbose` }),
-        new winston.transports.File({ filename: path.join(logPath, `combined.log`).normalize() }),
-        new winston.transports.Console({
-            format: winston.format.combine(
-                // label({ label: `amazon-invoice-craper` }),
-                winston.format.colorize(),
-                winston.format.simple(),
-                winston.format.splat(),
-                timestamp(),
-                myFormat
-            )
+
+export const createLogger = (logLevel: string, logPath: string, context: string = 'DocuDigger') => {
+    const logFormat = format.combine(
+        format.label({ label: context }),
+        format.timestamp({
+            format: 'YYYY-MM-DD HH-MM:ss'
         }),
-    ],
-});
+        format.prettyPrint(),
+        format.colorize(),
+        format.align(),
+        format.printf(info => {
+            return `[${info.timestamp}] [${info.label}] [${info.level}]: ${info.message}`;
+        })
+    );
+
+    return winston.createLogger({
+        level: logLevel.toString(),
+        format: logFormat,
+        transports:  [
+            new winston.transports.File({ filename: path.join(logPath, `error.log`).normalize(), level: `error` }),
+            new winston.transports.File({ filename: path.join(logPath, `verbose.log`).normalize(), level: `verbose` }),
+            new winston.transports.File({ filename: path.join(logPath, `combined.log`).normalize() }),
+            new winston.transports.Console()
+        ]
+    })
+};
