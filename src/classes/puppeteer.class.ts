44fa55cd8f58b0e764ff9c795df0248eb4da9b42
Marco Franke <disane@icloud.com>
2024-05-28 13:17:18 +0000

    fix: headless must be always true

--- src/classes/puppeteer.class.ts
+++ src/classes/puppeteer.class.ts
@@ -18,7 +18,7 @@ export class Puppeteer {
 
   public async setup(): Promise<Browser> {
     this.browser = await puppeteer.launch({
-      headless: this.inDocker ? true : (!this.debug ?? false),
+      headless: true,
       args: this.arguments,
       dumpio: false,
       devtools: this.debug,
