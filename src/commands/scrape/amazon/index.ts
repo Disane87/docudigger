dc6f6698beb910ba62b43704aad1cfd95a404d56
Alastair D'Silva <alastair@d-silva.org>
2025-05-03 20:09:02 +1000

    fix(tld): support the Australian Amazon site
    Signed-off-by: Alastair D'Silva <alastair@d-silva.org>

--- src/commands/scrape/amazon/index.ts
+++ src/commands/scrape/amazon/index.ts
@@ -319,13 +319,31 @@ export default class Amazon extends ScrapeCommand<typeof Amazon> {
     exit(this.logger, options.recurring);
   }
 
+  private getLoginPage(tld: string) {
+    switch (tld) {
+      case 'au':
+        return `https://www.amazon.com.au/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.au%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26signIn%3D1%26useRedirectOnSuccess%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=auflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0`;
+      default:
+        return `https://www.amazon.${tld}/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.de%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=deflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&`;
+    }
+  }
+
+  private getOrderPage(tld: string) {
+    switch (tld) {
+      case 'au':
+        return `https://www.amazon.com.au/gp/css/order-history`;
+      default:
+        return `https://www.amazon.${tld}/gp/css/order-history`;
+    }
+  }
+
   private getSelectors(tld: string): { amazonSelectors: AmazonSelectors, amazon: AmazonDefinition } {
     this.logger.debug(`Getting selectors...`);
     const amazon: AmazonDefinition = {
       lang: null,
       tld,
-      loginPage: `https://www.amazon.${tld}/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.de%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=deflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&`,
-      orderPage: `https://www.amazon.${tld}/gp/css/order-history`
+      loginPage: this.getLoginPage(tld),
+      orderPage: this.getOrderPage(tld)
     };
     return { amazonSelectors, amazon };
   }
